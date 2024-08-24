342dd49b167762ce291b0ebccb64a9669b2c4a7d
raka <rakapradipta.053@gmail.com>
2024-08-24 21:35:35 +0300

    apakek

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 const guessInput = document.getElementById('guessInput');
 const submitGuess = document.getElementById('submitGuess');
 const message = document.getElementById('message');
+let attempts = 5;
+
+function resetGame() {
+    attempts = 5;
+    document.getElementById('attempts').textContent = attempts;
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    message.textContent = '';
+    guessInput.value = '';
+}
 
 submitGuess.addEventListener('click', () => {
-    const userGuess = parseInt(guessInput.value);
-    if (userGuess === randomNumber) {
-        message.textContent = 'Selamat! Tebakkan kamu benar!';
-        message.style.color = 'green';
-    } else if (userGuess < randomNumber) {
-        message.textContent = 'Terlalu rendah! NT Coba lagi.';
-        message.style.color = 'orange';
-    } else if (userGuess > randomNumber) {
-        message.textContent = 'Terlalu tinggi! NT Coba lagi.';
-        message.style.color = 'orange';
+    if (attempts > 0) {
+        const userGuess = parseInt(guessInput.value);
+        attempts--;
+        document.getElementById('attempts').textContent = attempts;
+
+        if (userGuess === randomNumber) {
+            message.textContent = 'Selamat! Tebakkan kamu benar!';
+            message.style.color = 'green';
+            resetGame();
+        } else if (userGuess < randomNumber) {
+            message.textContent = 'Terlalu rendah! NT Coba lagi.';
+            message.style.color = 'orange';
+        } else if (userGuess > randomNumber) {
+            message.textContent = 'Terlalu tinggi! NT Coba lagi.';
+            message.style.color = 'orange';
+        }
+
+        if (attempts === 0) {
+            message.textContent = 'Kamu kalah cupu! Jawabannya ' + randomNumber + '.';
+            message.style.color = 'red';
+        }
+        
     }
 });
+
+document.getElementById('resetGame').addEventListener('click', resetGame);
