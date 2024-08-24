bbdf21e6e119534b1a3222ead727d98fffb59013
maaraap <rakapradipta.053@gmail.com>
2024-08-24 16:17:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const guessInput = document.getElementById('guessInput');
+const submitGuess = document.getElementById('submitGuess');
+const message = document.getElementById('message');
+
+submitGuess.addEventListener('click', () => {
+    
+    if (userGuess === randomNumber) {
+        message.textContent = 'Selamat! Tebakkan kamu benar!';
+        message.style.color = 'green';
+    } else if (userGuess < randomNumber) {
+        message.textContent = 'Terlalu rendah! NT Coba lagi.';
+        message.style.color = 'orange';
+    } else if (userGuess > randomNumber) {
+        message.textContent = 'Terlalu tinggi! NT Coba lagi.';
+        message.style.color = 'orange';
+    }
+});
